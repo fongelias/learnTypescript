d1467cf6a3a81e36ea0b14a09155aff71f8082b3
fongelias <fongelias@gmail.com>
2018-06-04 21:54:38 -0400

    added hello world example

---
+++ src/helloWorld/helloWorld.ts
@@ -0,0 +1,12 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Elias", lastName: "Fong" };
+
+console.log(greeter(user));
