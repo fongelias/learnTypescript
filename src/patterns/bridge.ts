100c5464c14abcc2f9359b3954356de511fe2abf
fongelias <fongelias@gmail.com>
2018-06-08 18:22:57 -0400

    added bridge design pattern

---
+++ src/patterns/bridge.ts
@@ -0,0 +1,44 @@
+import * as assert from 'assert';
+
+
+let externalBool = true;
+
+
+interface IAbstraction {
+	implementor: IImplementor;
+	abstractCall(): void;
+}
+
+interface IImplementor {
+	concreteFunction(): void;
+}
+
+class ConcreteAbstraction implements IAbstraction {
+	//Should be implemented to delegate to an Implementor class
+	implementor: IImplementor
+
+	constructor(impl: IImplementor) {
+		this.implementor = impl;
+	}
+
+	abstractCall() {
+		this.implementor.concreteFunction();
+	}
+}
+
+class ConcreteImplementor implements IImplementor {
+	concreteFunction() {
+		externalBool = !externalBool;
+	}
+}
+
+
+const concretion = new ConcreteImplementor();
+const abstraction = new ConcreteAbstraction(concretion);
+abstraction.abstractCall();
+assert(!externalBool);
+
+
+
+
+
