fe7cedef585d9b5c932f92108d148599b34bde04
fongelias <fongelias@gmail.com>
2018-06-17 09:33:54 +0200

    added comment on generics in hello component

--- src/react/hello.tsx
+++ src/react/hello.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export interface HelloProps { compiler: string; framework: string; }
 
 //When the constructor is not used, props and state can be passed in within <> brackets
+//The types in brackets replace generic types used by React.Component
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
     return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
